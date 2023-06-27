f5ccd2bf2b93b9650921235ef07eb17896f198bb
Abhishek Anand (optiplex) <abhishek.anand.iitg@gmail.com>
2023-06-27 16:31:51 -0400

    fixed the script

--- setup.sh
+++ setup.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
+set -x
 set -e
-PATH_IN_DOCKER=/home/coq/.opam/4.13.1+flambda/bin:/home/coq/.local/bin:/home/coq/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
-COQ_PREFIX=/home/coq/.opam/4.13.1+flambda/bin
+COQ_PREFIX=$OPAM_SWITCH_PREFIX
 docker pull coqorg/coq
+docker stop fmcontainerbhvbedrock &> /dev/null || true
 docker rm fmcontainerbhvbedrock &> /dev/null || true
 docker run --name fmcontainerbhvbedrock -d -ti coqorg/coq
 docker exec fmcontainerbhvbedrock git clone https://github.com/Matafou/coq -b fix-prompt-err-17753
+COQ_PREFIX=`docker exec fmcontainerbhvbedrock /bin/bash -c "source ~/.profile && printenv" | grep OPAM_SWITCH_PREFIX | cut -d '='  -f 2`
+docker exec -w /home/coq/coq fmcontainerbhvbedrock /bin/bash -c "source ~/.profile && ./configure -prefix=$COQ_PREFIX -no-ask"
+docker exec -w /home/coq/coq fmcontainerbhvbedrock /bin/bash -c "source ~/.profile && make dunestrap"
+docker exec -w /home/coq/coq fmcontainerbhvbedrock /bin/bash -c "source ~/.profile && dune build -p coq-core,coq-stdlib,coq,coqide-server"
+docker exec -w /home/coq/coq fmcontainerbhvbedrock /bin/bash -c "source ~/.profile && dune install --prefix=$COQ_PREFIX coq-core coq-stdlib coq coqide-server"
 
-docker exec -w /home/coq/coq -e PATH=$PATH_IN_DOCKER fmcontainerbhvbedrock  ./configure -prefix=$COQ_PREFIX -no-ask
-
-docker exec -w /home/coq/coq -e PATH=$PATH_IN_DOCKER fmcontainerbhvbedrock  make dunestrap
-
-docker exec -w /home/coq/coq -e PATH=$PATH_IN_DOCKER fmcontainerbhvbedrock dune build -p coq-core,coq-stdlib,coq,coqide-server
-
-docker exec -w /home/coq/coq -e PATH=$PATH_IN_DOCKER fmcontainerbhvbedrock dune install --prefix=$COQ_PREFIX coq-core coq-stdlib coq coqide-server
-
-#docker exec fmcontainerbhvbedrock sudo apt update
-#docker exec fmcontainerbhvbedrock sudo apt -y install expect
-#docker stop fmcontainerbhvbedrock
-#docker commit fmcontainerbhvbedrock coqunbuffer
-#docker rm fmcontainerbhvbedrock
+docker exec fmcontainerbhvbedrock sudo apt update
+docker exec fmcontainerbhvbedrock sudo apt -y install expect
+docker stop fmcontainerbhvbedrock
+docker commit fmcontainerbhvbedrock coqunbuffer
